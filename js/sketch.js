c3d61f85b89622a63ea9fc7f3907cc4251ea6c50
gillarious <isabella.berryx3@gmail.com>
2016-10-15 13:01:20 -0400

    lmao
    lmao

--- js/sketch.js
+++ js/sketch.js
@@ -4,10 +4,10 @@ function setup() {
 	var canvas = createCanvas(window.innerWidth, window.innerHeight);
 	canvas.parent('canvas-wrap');
 	smooth();
-	noStroke();
 }
 
 function draw() {
+	noStroke();
 	ellipse(x, height/2, 20, 20);
 	x = x + 1;
 }
