d0c1550d81b3c0677bc1a57fa9c455797df6acb1
gillarious <isabella.berryx3@gmail.com>
2016-10-15 12:09:35 -0400

    lmao
    lmao

--- js/sketch.js
+++ js/sketch.js
@@ -9,3 +9,11 @@ function draw() {
 	ellipse(x, height/2, 20, 20);
 	x = x + 1;
 }
+
+window.onresize = function() {
+	var w = window.innerWidth;
+	var h = window.innerHeight;  
+	canvas.size(w,h);
+	width = w;
+	height = h;
+};
