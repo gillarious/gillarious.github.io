a3de52112b8c51db8f7fc519a853b53d9a51cc89
isabella berry <isabella.berryx3@gmail.com>
2023-12-30 22:54:38 -0500

    been awhile oops

--- js/sketch.js
+++ js/sketch.js
@@ -1,64 +1,70 @@
+const WINDOW_EDGE = 25;
+const MAX_CIRCLES = 500;
+var numCircles = 0;
+var currentCircle = 0;
 var circles = [];
-var edge = 25;
+
+
+function windowResized() {
+	resizeCanvas(windowWidth-WINDOW_EDGE, windowHeight*5);
+}
+
 
 function setup() {
-	var canvas = createCanvas(windowWidth-edge, windowHeight*3);
+	var canvas = createCanvas(windowWidth-WINDOW_EDGE, windowHeight*3);
 	canvas.parent('canvas-wrap');
 	smooth();
 }
 
-function windowResized() {
-	resizeCanvas(windowWidth-edge, windowHeight*5);
-}
-
-var maxCircles = 500;
-var numCircles = 0;
-var currentCircle = 0;
 
 function draw() {
 	background(255);
-  
+
 	if (mouseIsPressed) {
-		for (var a = 0; a < 2; a++) {
+		for (let i = 0; i < 2; i++) {
 			circles.push(new Jitter());
 			currentCircle++;
-		} 
+		}
+
 		if (numCircles < circles.length){
 			numCircles++;
 		}
-		if (currentCircle >= maxCircles){
+
+		if (currentCircle >= MAX_CIRCLES){
 			currentCircle = 0;
 		}
 	}
-  
-	for (var i=0; i<circles.length; i++) {
+
+	for (let i = 0; i < circles.length; i++) {
 		circles[i].fade();
 		circles[i].move();
 		circles[i].display();
 	}
-}
+};
 
-function Jitter() {
-	this.x = mouseX;
-	this.y = mouseY;
- 	this.diameter = random(15, 35);
- 	this.speedX = random(-3, 3);
-	this.speedY = random(-3, 3);
-	this.myRed = 150;
-	this.myGreen = random(200, 255);
-	this.myBlue = random(0, 100);
-	this.myAlpha = random(0, 255);
+class Jitter {
+	constructor() {
+		this.x = mouseX;
+		this.y = mouseY;
+		this.diameter = random(15, 35);
+		this.speedX = random(-3, 3);
+		this.speedY = random(-3, 3);
+		this.myRed = 150;
+		this.myGreen = random(200, 255);
+		this.myBlue = random(0, 100);
+		this.myAlpha = random(0, 255);
+	};
 
-	this.move = function() {
+	move() {
 		this.x += this.speedX;
 		this.y += this.speedY;
-    
-		if (this.x > windowWidth-edge){
-			this.x = windowWidth-edge;
+
+		if (this.x > windowWidth-WINDOW_EDGE){
+			this.x = windowWidth-WINDOW_EDGE;
 			this.speedX *= -1;
 		}
-		if (this.y > (windowHeight*3)-edge){
-			this.y = (windowHeight*3)-edge;
+		if (this.y > (windowHeight*3)-WINDOW_EDGE){
+			this.y = (windowHeight*3)-WINDOW_EDGE;
 			this.speedY *= -1;
 		}
 		if (this.x < 0){
@@ -69,20 +75,19 @@ function Jitter() {
 			this.y = 0;
 			this.speedY *= -1;
 		}
-  	}
+  	};
 
-	this.display = function() {
+	display() {
 		noStroke();
 		fill(this.myRed, this.myGreen, this.myBlue, this.myAlpha);
 		ellipse(this.x, this.y, this.diameter, this.diameter);
 	};
-  
-	this.fade = function() {
-		if (this.myAlpha > 0){
+
+	fade() {
+		if (this.myAlpha > 0) {
 			this.myAlpha -= 2;
-		}
-		else{
+		} else {
 			this.myAlpha = 0;
 		}
-	}
-}
+	};
+};
